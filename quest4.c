1dd19a6077a924fad27a05304229cd6a1857abe0
lamichhanes2 <lamichhanes2@csc4252021.ssdt.berea.edu>
2021-09-03 13:24:05 -0400

    working on quest4.c

---
+++ quest4.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+
+
+int main(int argc, char *argv[]){
+
+ rc = fork()
+
+ if (rc == 0){
+    char *myargs[3];
+    myargs[0] = strdup("/bin/ls");
+    myargs[1] = 
+}
+
+}
